92cac80325aa57edcd07b637200d16ca0fb7e8db
Trung1234 <trungsieunhan24@gmail.com>
2019-08-24 22:47:26 +0700

    update crud

---
+++ EmployeeTable/EmployeeTable/Data/EmployeeTableContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeTable.Models
+{
+    public class EmployeeTableContext : DbContext
+    {
+        public EmployeeTableContext (DbContextOptions<EmployeeTableContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmployeeTable.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ EmployeeTable/EmployeeTable/Migrations/20190818104539_Trung.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeTable.Migrations
+{
+    public partial class Trung : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 255, nullable: false),
+                    Salary = table.Column<float>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    Status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
